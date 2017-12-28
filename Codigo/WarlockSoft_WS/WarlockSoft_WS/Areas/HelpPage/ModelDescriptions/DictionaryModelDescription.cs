a4cd315e46bb88d0d8eede5d79e2094acec4330e
Andre Reyna <deleonandree@gmail.com>
2017-12-28 09:25:20 -0600

    C# ws

---
+++ Codigo/WarlockSoft_WS/WarlockSoft_WS/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WarlockSoft_WS.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Codigo/WarlockSoft_WS/WarlockSoft_WS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WarlockSoft_WS.WebApiApplication" Language="C#" %>
